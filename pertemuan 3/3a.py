b76bf0ed10092463c05b487b4c1940238bec191c
NaylaSabryna <geanandasabryna@gmail.com>
2026-06-05 20:28:41 +0700

    Add files via upload

---
+++ pertemuan 3/3a.py
@@ -0,0 +1,53 @@
+#no.01
+var = 20
+nilai = 90
+nama_mahasiswa = "Nayla Sabryna"
+
+print(var)
+print(nilai)
+print(nama_mahasiswa)
+print(var,nilai,nama_mahasiswa)
+
+#no.02
+nama = "Budi"
+print(nama)
+
+#no.03
+nama = "Nayla"
+umur = 17
+print (nama)
+print (umur)
+
+#no.04
+nama = "Nayla"
+umur = 17
+tabungan = 100000
+print (nama)
+print (umur)
+print (tabungan)
+
+#no.05
+nama = "Nayla"
+umur = 17
+tabungan = 100000
+print (nama)
+print (umur)
+print (tabungan)
+
+#no.06
+a = 3.0
+b = 4.0
+c = (a**2 + b**2)**0.5
+print("c=",c)
+
+#no.07
+a = 3.0
+b = 4.0
+c = (a**2 + b**2)**0.5
+print("c=",c)
+
+#no.08
+nilai_x = [0,1,-1]
+for x in nilai_x :
+   y = 3*x**3 -2*x**2 + 3*x -1
+   print("jika X =", "maka y = ",y)
