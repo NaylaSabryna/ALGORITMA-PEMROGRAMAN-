771df994785b0aee39250b8e01ab309b9ec88c91
NaylaSabryna <geanandasabryna@gmail.com>
2026-06-05 20:27:25 +0700

    Add files via upload

---
+++ pertemuan 2/1a.py
@@ -0,0 +1,104 @@
+#no.01
+nama = "naylaa" #string literal
+umur = "12" #integer literal (catatan: karena pakai kutub, ini dibaca string)
+th = "tahun" #string literal
+satuan = "cm" #string literal
+tinggi = "150" #float literal (catatan: karena pakai kutub, ini dibaca string)
+
+print ("nama:", nama ) #mencetak teks nama: dan akan diisi dari variabel nama
+print ("umur:", umur, th) #mencetak teks umur: dan akan diisi dari variabel umur dan th
+print ("tinggi:", tinggi, satuan) #mencetak teks tinggi: dan akan diisi dari variabel tinggi dan satuan
+
+#no.02
+angkaPositif = 11111111  #literal integer
+angkaNegatif = -11111111 #literal integer
+print("Angka Positif :", angkaPositif) #mencetak teks angka positif : dan akan diisi dari variabel angkaPositif
+print("Angka Negatif :", angkaNegatif) #mencetak teks angka negatif : dan akan diisi dari variabel angkaNegatif
+
+#no.03
+print (0o25) #mencetak nilai bilangan octa
+print (0x19) #mencetak nilai bilangan hexadecimal
+
+#n0.04
+#Fungsi print dengan literal float
+print(0.4)
+print(4.0)
+
+#Bentuk float tanpa angka 0 di depan (menggunakan notasi singkat)
+print(.4)
+print(4.)
+
+#float dengan notasi eksponansial
+print(1.5e1)  #15.0
+print(2.5e-1) #0.25
+print(3.0e+2) #300.0
+
+
+#no.05
+print ("selamat pagi dunia, hai... ") #mencetak literal string menggunakan petik 2 ("")
+print ('selamat siang dunia, hai... ') #mencetak literal string menggunakan petik 1 (')
+
+#no.06
+print (True) #mencetak literal boolean True
+print (False) #mencetak literal boolean False
+
+#no07
+print (4 ** 7) #pengoperasian pemangkatan integer-integer
+print (4 ** 7.5) #pengoperasian pemangkatan integer-float
+print (4.5 ** 7) #prngoperasiam pemangkatan float-integer
+print (4.5 ** 7.5) #pengoperasian pemangkatan float-float
+
+#no.08
+print(2 * 6) #pengoperasian perkalian integer-integer
+print(2 * 6.0) #pengoperasian perkalian integer-float
+print(2.0 * 6) #pengoperasian perkalian float-integer
+print(2.0 * 6.0) #pengoperasian perkalian float-float
+
+#no.09
+#Pembagian integer-integer
+print(10/2)
+#Pembagian integer-float
+print(10/2.5)
+#Pembagian float-integer
+print(10.5/2)
+#Pembagian float-float
+print(10.5/2.5)
+
+#no.10
+print(10//2) #Pembagian integer-integer
+print(10//2.5) #Pembagian integer-float
+print(10.5//2) #Pembagian float-integer
+print(10.5//2.5) #Pembagian float-float
+
+#no.11
+print(10//2) #Pembagian integer-integer
+print(10//2.5) #Pembagian integer-float
+print(10.5//2) #Pembagian float-integer
+print(10.5//2.5) #Pembagian float-float
+
+#no.12
+#Operator unary
+print(-2) #Operator unary untuk negasi angka
+print(+3) #Operator unary untuk angka positif
+print(not True) #Operator unary untuk negasi boolean 
+print(not False) #Operator unary untuk negasi boolean
+
+#Operator Binary
+print(5+5) #Operator binary untuk penjumlahan
+print(5-3) #Operator binary untuk pengurangan
+print(8*3) #Operator binary untuk perkalian
+print(24/3) #Operator binary untuk pembagian
+print(10//3) #Operator binary untuk pembagian integer
+print(72%3) #Operator binary untuk modulo (sisa bagi)
+print(4**3) #OPerator binary untuk pemangkatan
+
+#no.13
+#Fungsi Print Subekspresi
+print(5*(5+5)) #Fungsi print untuk mencetak hasl dari subekspresi 5+5 dan kemudian dikalikan dengan 5
+print((30-6)/2) #FungsiFungsi print untuk mencetak hasil dari 30-6 dan kemudian dibagi 2
+
+#no.14
+#Fungsi Print Subekspresi
+print(5*(5+5)) #Fungsi print untuk mencetak hasl dari subekspresi 5+5 dan kemudian dikalikan dengan 5
+print((30-6)/2) #FungsiFungsi print untuk mencetak hasil dari 30-6 dan kemudian dibagi 2
+
